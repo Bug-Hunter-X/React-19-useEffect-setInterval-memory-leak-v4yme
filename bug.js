bf8f9ce98a75f384a1f38f32f46e30c26c8b17df
BugHunter-X <askzenai@gmail.com>
2024-12-10 11:32:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect usage of setInterval. Missing cleanup function
+    const intervalId = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+  }, []);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
